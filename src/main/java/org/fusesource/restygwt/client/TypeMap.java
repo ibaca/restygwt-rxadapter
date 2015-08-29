4a9d9bbddb47591982cadd5a29a324b3daa11e77
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2015-08-29 10:28:32 +0200

    @TypeMap annotation

---
+++ src/main/java/org/fusesource/restygwt/client/TypeMap.java
@@ -0,0 +1,13 @@
+package org.fusesource.restygwt.client;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TypeMap {
+    Class<?> type();
+
+    Class<?> with();
+}
